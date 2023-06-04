aef8939059a57982fcf6053c77c2a370e46c8aa8
feynbin <feynbin@outlook.com>
2023-06-05 01:02:07 +0800

    练习了time库

--- demo1.go
+++ demo1.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println("Hello World")
+	fmt.Println("Hello World")
+	end := time.Now()
+	fmt.Println(end.Sub(start))
+	current_time := time.Now()
+	fmt.Println(current_time)
+	fmt.Println(current_time.Format(time.DateTime))
+	fmt.Println(time.DateTime)
+
 }
